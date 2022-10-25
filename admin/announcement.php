b367ed4ba1628ccf27f25e44c6df66746d4230dd
Shakthi Vignesh J <87350662+Vignesh-2811@users.noreply.github.com>
2022-10-26 01:50:28 +0530

    modified admin pages

---
+++ admin/announcement.php
@@ -0,0 +1,64 @@
+<?php
+include('includes/header.php');
+include('../functions/myfunctions.php');
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Announcements</h4>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Poster</th>
+                                <th>publish</th>
+                                <th>Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $announcement = getAll("announcement");
+                            
+                            if(mysqli_num_rows($announcement) > 0)
+                            {
+                                foreach($announcement as $item){
+                                    ?>
+                                    <tr>
+                                        <td><?= $item['id']; ?></td>
+                                        <td><?= $item['title'] ?></td>
+                                        <td>
+                                            <img src="../uploads/<?= $item['poster'] ?>" width="50px" height="50px" alt="<?= $item['poster'] ?>">
+                                        </td>
+                                        <td>
+                                            <?= $item['publish'] == '1'? "Visible":"Hidden" ?>
+                                        </td>
+
+                                        <td>
+                                            <a href="#" class="btn btn-primary">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                            else{
+                                echo "No records found";
+                            }
+                                
+                                ?>
+                           
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php');
--- functions/myfunctions.php
+++ functions/myfunctions.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
+include('../config/connection.php');
+
+function getAll($table)
+{
+    global $conn;
+    $query = "SELECT * FROM $table";
+    return $query_run = mysqli_query($conn, $query);
+}
+
 
 function redirect($url, $message)
 {
     $_SESSION['message'] = $message;
     header('Location: '.$url);
     exit();
-
 }
 
 ?>
